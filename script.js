95b45d0bf82b886dd4163aa4dcf9c9304d6f74a0
Fengler111 <164820646+Fengler111@users.noreply.github.com>
2024-04-09 10:37:10 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('replace-button').addEventListener('click', function() {  
+    document.getElementById('centered-image').src = '2221.jpg'; //Replace with a new image path 
+});
